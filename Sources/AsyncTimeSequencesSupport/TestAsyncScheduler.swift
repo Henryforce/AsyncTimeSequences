4cc37aa51ce007aa2c281715eb14773a8051968a
Henryforce <hj_serrano@hotmail.com>
2022-01-12 17:51:22 +0900

    Tests docs (#8)
    * Updated README and test documentation
    
    * removed git workflow

--- Sources/AsyncTimeSequencesSupport/TestAsyncScheduler.swift
+++ Sources/AsyncTimeSequencesSupport/TestAsyncScheduler.swift
@@ -9,6 +9,7 @@ import Foundation
 import AsyncTimeSequences
 import DataStructures
 
+/// This class conforms to AsyncScheduler and provides convenient functions to easily test async time sequences.
 public actor TestAsyncScheduler: AsyncScheduler {
     
     struct QueueItem {
@@ -24,6 +25,10 @@ public actor TestAsyncScheduler: AsyncScheduler {
     
     public init() { }
     
+    /// Schedule a specific async handler to be executed after interval.
+    /// All the async handler will be enqueued until advance() is called for processing.
+    /// This function will also check if n jobs have been scheduled and try to resume a
+    /// saved continuation set during waitForScheduledJobs().
     public func schedule(after interval: TimeInterval, handler: @escaping AsyncSchedulerHandler) {
         let item = QueueItem(interval: interval + now, handler: handler)
         queue.enqueue(item)
@@ -31,6 +36,8 @@ public actor TestAsyncScheduler: AsyncScheduler {
         checkForSavedContinuation()
     }
     
+    /// Advances local time interval and executes all enqueued async handlers that are
+    /// contained within the advanced interval.
     public func advance(by interval: TimeInterval) async {
         let threshold = now + interval
         now = threshold
@@ -39,9 +46,10 @@ public actor TestAsyncScheduler: AsyncScheduler {
               let item = queue.dequeue() {
             await item.handler()
         }
-        
     }
     
+    /// This function waits until n jobs are scheduled via the schedule() function.
+    /// If there are already n jobs scheduled, this function will return immediately.
     public func waitForScheduledJobs(count: Int) async {
         await withCheckedContinuation({ (continuation: CheckedContinuation<Void, Never>) in
             if (queue.count >= count) {
